6d6103dcb265ec41b093ec06d3f51e1edf45c480
Sara Cristina Moura Faria <sara.faria@luizalabs.com>
2020-06-15 21:51:51 -0300

    init Spring

---
+++ src/test/java/com/spring/course/api/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.course.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
